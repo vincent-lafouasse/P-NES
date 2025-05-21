a7cd5f4f876387c44fa18aad0cb66fcef576ca2b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-21 21:28:05 +0200

    take file path from sysargs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
 #include "parse/GameData.hpp"
 #include "types.hpp"
 
-#define PATH "./roms/dq1.nes"
+int main(int ac, char** av) {
+    if (ac == 1) {
+        std::cerr << "Usage: ./Nes rom.nes\n";
+        std::exit(1);
+    }
 
-int main() {
-    ByteStream stream(PATH);
+    const char* path = av[1];
+    ByteStream stream(path);
+    if (!stream.good()) {
+        std::cerr << "Failed to open file " << std::quoted(path) << '\n';
+        std::exit(1);
+    }
 
     GameData g = GameData::read(stream);
     g.dump_prg();
