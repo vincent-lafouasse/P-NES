2b68a4b31e6b0cabdeeb69c5c13e3d88d6569a46
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-22 18:54:39 +0200

    sketch Cpu interface

--- src/Cpu/Cpu.hpp
+++ src/Cpu/Cpu.hpp
@@ -3,24 +3,32 @@
 #include "types.hpp"
 
 class Cpu final {
-    [[maybe_unused]] Byte A;
-    [[maybe_unused]] Byte X;
-    [[maybe_unused]] Byte Y;
-    [[maybe_unused]] Byte SP;
-    [[maybe_unused]] Address PC;
-    [[maybe_unused]] Byte P;
+   public:
+    Cpu() = default;
+    void reset();
 
-    /*
-        namespace Status {
-        bool
-        N	Negative
-        V	Overflow
-        -	ignored
-        B	Break
-        D	Decimal (use BCD for arithmetics)
-        I	Interrupt (IRQ disable)
-        Z	Zero
-        C	Carry
-        }
-        */
+   private:
+    Byte accumulator;
+    Byte x_register;
+    Byte y_register;
+    Byte stack_pointer;
+    Address program_counter;
+    struct Status {
+        // this whole thing should take up a byte
+        unsigned char carry : 1;
+        unsigned char zero_flag : 1;
+        unsigned char interrupt_flag : 1;
+        unsigned char decimal_flag : 1;
+        unsigned char break_flag : 1;
+        unsigned char padding : 1;
+        unsigned char overflow_flag : 1;
+        unsigned char negative_flag : 1;
+    } status;
+
+    auto& A() { return accumulator; }
+    auto& X() { return x_register; }
+    auto& Y() { return y_register; }
+    auto& S() { return stack_pointer; }
+    auto& PC() { return program_counter; }
+    auto& P() { return status; }
 };
