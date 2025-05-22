acd8564c3c171c539cbd72b014b0b9dd4ccb14fd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-22 22:50:58 +0200

    store instruction set info in enums

--- src/Cpu/Cpu.hpp
+++ src/Cpu/Cpu.hpp
@@ -5,11 +5,68 @@
 
 struct Instruction {
     enum class Kind {
-        SEI,
+        // Transfer instructions
+        Load_A,
+        Load_X,
+        Load_Y,
+        Store_A,
+        Store_X,
+        Store_Y,
+        Transfer_A2X,
+        Transfer_A2Y,
+        Transfer_S2X,
+        Transfer_X2A,
+        Transfer_X2S,
+        Transfer_Y2A,
+        // Stack instructions
+        Push_A,
+        Push_P,
+        Pull_A,
+        Pull_P,
+        // Decrements/Increments
+        Decrement_Mem,
+        Decrement_X,
+        Decrement_Y,
+        Increment_Mem,
+        Increment_X,
+        Increment_Y,
+        // Arithmetic
+        AddWithCarry,
+        SubtractWithCarry,
+        // Logic
+        And_A,
+        Xor_A,
+        Or_A,
+        // Shift/Rotate
+        ArithmeticShift_Left,
+        LogicalShift_Left,
+        Rotate_Left,
+        Rotate_Right,
+        // Flags
+        Clear_Carry,
+        Clear_Decimal,
+        Clear_Interrupt,
+        Clear_Overflow,
+        Set_Carry,
+        Set_Decimal,
+        Set_Interrupt,
+        // Unrecognized opcode
         Unknown,
     } kind;
     enum class Mode {
+        Accumulator,
+        Absolute,
+        Absolute_X,
+        Absolute_Y,
+        Immediate,
         Implied,
+        Indirect,
+        X_Indirect,
+        Indirect_Y,
+        Relative,
+        ZeroPage,
+        ZeroPage_X,
+        ZeroPage_Y,
     } mode;
     usize size;
     usize cycles;
@@ -23,8 +80,10 @@ struct Instruction {
         }
     }
 
-    static Instruction SEI() { return {Kind::SEI, Mode::Implied, 1, 2}; }
-    static Instruction Unkown() { return {Kind::Unknown}; }
+    static Instruction SEI() {
+        return {Kind::Set_Interrupt, Mode::Implied, 1, 2};
+    }
+    static Instruction Unknown() { return {Kind::Unknown}; }
 };
 
 class Cpu final {
