be693170f9183fb7868dd7acbe2b8832d61147fe
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-23 14:33:48 +0200

    refactor

--- src/Cpu/Instruction.hpp
+++ src/Cpu/Instruction.hpp
@@ -4,95 +4,101 @@
 
 #include "types.hpp"
 
-struct Instruction {
-    enum class Kind {
-        // Transfer instructions
-        Load_A,
-        Load_X,
-        Load_Y,
-        Store_A,
-        Store_X,
-        Store_Y,
-        Transfer_A2X,
-        Transfer_A2Y,
-        Transfer_S2X,
-        Transfer_X2A,
-        Transfer_X2S,
-        Transfer_Y2A,
-        // Stack instructions
-        Push_A,
-        Push_P,
-        Pull_A,
-        Pull_P,
-        // Decrements/Increments
-        Decrement_Mem,
-        Decrement_X,
-        Decrement_Y,
-        Increment_Mem,
-        Increment_X,
-        Increment_Y,
-        // Arithmetic
-        AddWithCarry,
-        SubtractWithCarry,
-        // Logic
-        And_A,
-        Xor_A,
-        Or_A,
-        // Shift/Rotate
-        ArithmeticShift_Left,
-        LogicalShift_Right,
-        Rotate_Left,
-        Rotate_Right,
-        // Flags
-        Clear_Carry,
-        Clear_Decimal,
-        Clear_Interrupt,
-        Clear_Overflow,
-        Set_Carry,
-        Set_Decimal,
-        Set_Interrupt,
-        // Comparison
-        Compare_A,
-        Compare_X,
-        Compare_Y,
-        // Branches
-        Branch_CarryClear,
-        Branch_CarrySet,
-        Branch_Equal,
-        Branch_NotEqual,
-        Branch_Minus,
-        Branch_Plus,
-        Branch_OverflowClear,
-        Branch_OverflowSet,
-        // Jumps/Subroutines
-        Jump,
-        Jump_Subroutine,
-        Return_Subroutine,
-        // Breaks/Interrupts
-        Break,
-        Return_Interrupt,
-        // Other
-        BitTest,
-        NoOp,
-        // Unrecognized opcode
-        Unknown,
-    } kind;
+enum class InstructionKind {
+    // Transfer instructions
+    Load_A,
+    Load_X,
+    Load_Y,
+    Store_A,
+    Store_X,
+    Store_Y,
+    Transfer_A2X,
+    Transfer_A2Y,
+    Transfer_S2X,
+    Transfer_X2A,
+    Transfer_X2S,
+    Transfer_Y2A,
+    // Stack instructions
+    Push_A,
+    Push_P,
+    Pull_A,
+    Pull_P,
+    // Decrements/Increments
+    Decrement_Mem,
+    Decrement_X,
+    Decrement_Y,
+    Increment_Mem,
+    Increment_X,
+    Increment_Y,
+    // Arithmetic
+    AddWithCarry,
+    SubtractWithCarry,
+    // Logic
+    And_A,
+    Xor_A,
+    Or_A,
+    // Shift/Rotate
+    ArithmeticShift_Left,
+    LogicalShift_Right,
+    Rotate_Left,
+    Rotate_Right,
+    // Flags
+    Clear_Carry,
+    Clear_Decimal,
+    Clear_Interrupt,
+    Clear_Overflow,
+    Set_Carry,
+    Set_Decimal,
+    Set_Interrupt,
+    // Comparison
+    Compare_A,
+    Compare_X,
+    Compare_Y,
+    // Branches
+    Branch_CarryClear,
+    Branch_CarrySet,
+    Branch_Equal,
+    Branch_NotEqual,
+    Branch_Minus,
+    Branch_Plus,
+    Branch_OverflowClear,
+    Branch_OverflowSet,
+    // Jumps/Subroutines
+    Jump,
+    Jump_Subroutine,
+    Return_Subroutine,
+    // Breaks/Interrupts
+    Break,
+    Return_Interrupt,
+    // Other
+    BitTest,
+    NoOp,
+    // Unrecognized opcode
+    Unknown,
+};
 
-    enum class Mode {
-        Accumulator,
-        Absolute,
-        Absolute_X,
-        Absolute_Y,
-        Immediate,
-        Implied,
-        Indirect,
-        X_Indirect,
-        Indirect_Y,
-        Relative,
-        ZeroPage,
-        ZeroPage_X,
-        ZeroPage_Y,
-    } mode;
+enum class InstructionMode {
+    Accumulator,
+    Absolute,
+    Absolute_X,
+    Absolute_Y,
+    Immediate,
+    Implied,
+    Indirect,
+    X_Indirect,
+    Indirect_Y,
+    Relative,
+    ZeroPage,
+    ZeroPage_X,
+    ZeroPage_Y,
+};
+
+struct Instruction {
+    using Kind = InstructionKind;
+    using Mode = InstructionMode;
+
+    Kind kind;
+    Mode mode;
     usize size;
     usize cycles;
 
