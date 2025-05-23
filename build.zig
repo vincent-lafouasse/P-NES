85d29bfb2c7f818e9ca0f664ae74517ea2b7fe66
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-23 18:46:00 +0200

    create main module

--- build.zig
+++ build.zig
@@ -5,11 +5,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "Nes",
+    const exe_mod = b.createModule(.{
         .root_source_file = b.path("./src/main.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "Nes",
+        .root_module = exe_mod,
     });
 
     b.installArtifact(exe);
