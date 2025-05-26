fcac1f1c99d2a1d8df8777d5bcf4230a80be7250
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-26 21:18:29 +0200

    init CPU RAM

--- src/Bus.zig
+++ src/Bus.zig
@@ -1,23 +1,29 @@
 const std = @import("std");
+const ByteList = std.ArrayList(u8);
 const Allocator = std.mem.Allocator;
 
 const Cartridge = @import("Cartridge.zig").Cartridge;
 
 pub const Bus = struct {
+    const cpuRamSize = 0x800; // 2kB
+    cpuRam: ByteList,
+
     cartridge: *const Cartridge,
 
     const Self = @This();
 
     pub fn init(allocator: Allocator, cartridge: *const Cartridge) !Self {
-        _ = allocator;
+        var cpuRam = try ByteList.initCapacity(allocator, Bus.cpuRamSize);
+        cpuRam.appendNTimesAssumeCapacity(0, Bus.cpuRamSize);
 
         return Self{
             .cartridge = cartridge,
+            .cpuRam = cpuRam,
         };
     }
 
     pub fn free(self: Self) void {
-        _ = self;
+        self.cpuRam.deinit();
     }
 
     pub fn read(self: *const Self, address: u16) u8 {
