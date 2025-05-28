048587eaa3dafaf0f87f6bbce16f953014676e8d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-28 21:21:37 +0200

    restructure

--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const cartridge = try Cartridge.load("roms/nestest/nestest.nes", allocator);
+    const cartridge = try Cartridge.load("./aux/roms/nestest/nestest.nes", allocator);
     defer cartridge.free();
 
     cartridge.log();
